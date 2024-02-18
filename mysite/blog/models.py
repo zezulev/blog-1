1a4813f6d2cec1dfe9d39f230f4bfb29a274a004
rostik95 <danver1995@mail.ru>
2024-02-18 12:03:52 +0300

    created model and migration for Post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
+
+    class Meta:
+        ordering = ['-publish']
+    indexes = [
+        models.Index(fields=['-publish'])
+    ]
+
+    def __str__(self):
+        return self.title
