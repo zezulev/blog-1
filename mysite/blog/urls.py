88ee47e3fdc7bb1948a6dfaefac42a8e661a7113
rostik95 <danver1995@mail.ru>
2024-02-25 12:31:41 +0300

    added html pages

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('hello/', views.hello, name='hello'),
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
+from django.http import Http404
 
 
 # Create your views here.
@@ -9,3 +11,12 @@ def index(request):
 
 def hello(request):
     return HttpResponse('Hello world!')
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
